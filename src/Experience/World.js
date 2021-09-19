cc4aaf98dbe9992152ffd84c731c23acda6a6ccd
sangnt.2307 <sangnt.2307@gmail.com>
2021-09-19 16:03:45 +0700

    first-commit

---
+++ src/Experience/World.js
@@ -0,0 +1,48 @@
+import * as THREE from 'three'
+import Experience from './Experience.js'
+
+export default class World
+{
+    constructor(_options)
+    {
+        this.experience = new Experience()
+        this.config = this.experience.config
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        
+        this.resources.on('groupEnd', (_group) =>
+        {
+            if(_group.name === 'base')
+            {
+                this.setDummy()
+                // this.setRoom()
+            }
+        })
+    }
+
+    setDummy()
+    {
+        const cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({ map: this.resources.items.lennaTexture })
+        )
+        this.scene.add(cube)        
+    }
+
+    // setRoom()
+    // {
+
+    // }
+
+    resize()
+    {
+    }
+
+    update()
+    {
+    }
+
+    destroy()
+    {
+    }
+}
